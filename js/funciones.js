6a5e66b34c683920deb4b188daff9f1dd30fd8f4
publica <rogersito177@gmail.com>
2015-10-15 17:22:57 -0700

    primer commit

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+function generar()
+{
+   var numper=$("#persona").val();
+   var numgrup=$("#grupo").val();
+   alert (numper/numgrup)
+
+    var mu=0;
+    var ar=[];
+    for (i=1;i<=numper;i++)
+    {
+    ar.push(i);
+    }
+    alert(ar);
+}
+
